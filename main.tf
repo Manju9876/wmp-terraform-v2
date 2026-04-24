39a6b2ba803f5d54345642d9990e5db9ad65b222
Manju9876 <manjunathb960650@gmail.com>
2026-04-24 12:39:09 +0530

    github actions practice

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ module "component" {
   source = "./modules/component"
   for_each = var.component_name
 
-  ami_id         = each.key["ami_id"]
+  ami_id         = each.value["ami_id"]
   component_name = each.key
   env            = var.env
   zone_id        = var.zone_id
-  instance_type = each.key["instance_type"]
+  instance_type = each.value["instance_type"]
 }
